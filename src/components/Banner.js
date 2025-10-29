52c37d389f967063c5841cc471bb0830abe9aa5a
kimm310 <akaneemagi@gmail.com>
2025-10-29 20:28:22 +0100

    Created banner and project cards

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,12 +5,10 @@ import headerImg from  "../assets/img/header.png"
 
 export const Banner = () => {
 
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleted, setIsDeleted] = useState(false);
-    const toRotate = ["Web Developer", "App Developer"];
-    const [text, setText] = useState('');
-    const period = 2000;    //
-    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [loopNum, setLoopNum] = useState(0);  // indicates which word is currently displayed
+    const toRotate = ["Hi, I'm Kim"];
+    const [text, setText] = useState('');   // indicates the portion of the word being displayed
+    const [delta, setDelta] = useState(300 - Math.random() * 100);  // determines time passed between letters
 
     useEffect(() => {
         let ticker = setInterval(() => {
@@ -20,18 +18,26 @@ export const Banner = () => {
         return () => {clearInterval(ticker)}
     }, [text])
 
+    const tick = () => {
+        let i = loopNum % toRotate.length;  // we increase the index by 1 but only have two words so when loopNum equals 3, we mod it to get 1 again
+        let fullText = toRotate[i];
+        let updatedText = fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+    }
+
     return (
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm webcoded`}</h1>
-                        <p>This is a text where I say stuff about myself.</p>
-                        <button onClick={() => console.log('connect')}>Let's connect</button>
+                        <h1>{``}<span className="wrap">{text}</span></h1>
+                        <p>I am an aspiring software developer currently exploring frontend web development in React.</p>
+                        <button onClick={() => console.log('connect')}>Let's connect<ArrowRightCircle size={25}/></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={headerImg} alt="cool cat"/>
+                        <img src={headerImg} alt="cool cat" style={{ opacity: 0 }}/>    {/*For now, make it invisible*/}
                     </Col>
                 </Row>
             </Container>
---
+++ src/components/Projects.js
@@ -0,0 +1,62 @@
+import projImg1 from '../assets/img/project-img1.png';
+import projImg2 from '../assets/img/project-img2.png';
+import {ProjectCard} from './ProjectCards';
+import colorSharp2 from "../assets/img/color-sharp2.png"
+import {Col, Container, Nav, Row, Tab} from "react-bootstrap";
+
+export const Projects = () => {
+    const projects = [
+        {
+            title: "Fitness App",
+            description: "Android mobile app that tracks your physical and mental health",
+            imgUrl: projImg1
+        },
+        {
+            title: "Traffic Photo-Analysis App",
+            description: "Android mobile app that accesses your phone camera and sends pictures to ChatGPT for analysis",
+            imgUrl: projImg2
+        }
+    ];
+
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Projects</h2>
+                        <p>Most of my programming experiences come from student projects.</p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+
+                        <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                            <Nav.Item>
+                                <Nav.Link eventKey="first">Tab One</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="first">
+                                <Row>
+                                    {
+                                        projects.map((project, index) => {
+                                            return (
+                                                <ProjectCard key={index} {...project}/>
+                                            )
+                                        })
+                                    }
+                                </Row>
+                            </Tab.Pane>
+                            <Tab.Pane eventKey="second">
+                                <p>Meowmeowmeow</p>
+                            </Tab.Pane>
+                        </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2}/>
+        </section>
+    )
+
+}
